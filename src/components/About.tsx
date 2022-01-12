9ff4d293bacef001d0f21d1dd04cc2e6a705fe09
Sina Moradian <simora@microsoft.com>
2022-01-11 20:41:54 -0800

    .

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export function About() {
   return (
     <div>
-    <h1>Home</h1>
+    <h1>Aboot</h1>
     <nav>
       <Link to="/">Home</Link> | {" "}
       <Link to="/about">About</Link>
